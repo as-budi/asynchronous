708ad835cbcc87f0ebed704299cbcb41e8866d1b
as-budi <agungsetiabudi@ub.ac.id>
2024-05-28 08:35:05 +0700

    execute 2 async_sleep in main

--- async_sleep.py
+++ async_sleep.py
@@ -1,18 +1,18 @@
 import asyncio
 import time
 
-async def async_sleep():
-    print("Before sleep")
+async def async_sleep(n):
+    print("Before sleep", n)
     await asyncio.sleep(5)
-    print("After sleep")
+    print("After sleep", n)
 
-async def hello():
-    return 'Hello'
+async def async_hello():
+    print('Hello')
 
 async def main():
-    await async_sleep()
-    result = await hello()
-    print(result)
+    await async_sleep(1)
+    await async_sleep(2)
+    await async_hello()
 
 if __name__ == "__main__":
     s = time.time()
