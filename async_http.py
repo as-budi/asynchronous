c8f9a01839f1b5ef2f7800179ccfb5f93b181dda
as-budi <agungsetiabudi@ub.ac.id>
2024-05-28 09:31:29 +0700

    use aiohttp to perform http request asynchronously

--- async_http.py
+++ async_http.py
@@ -1,6 +1,12 @@
 import asyncio
 import time
 import requests
+import aiohttp
+
+async def get_url_response(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
 
 async def main():
     urls = ['https://google.com',
@@ -12,11 +18,21 @@ async def main():
             'https://github.com']
     
     start = time.time()
+    sync_text_response = []
     for url in urls:
-        requests.get(url)
+        sync_text_response.append(requests.get(url).text)
     end_time = time.time()
     print('Request took:', end_time - start)
 
+    start = time.time()
+    tasks = []
+    for url in urls:
+        tasks.append(asyncio.create_task(get_url_response(url)))
+    async_text_response = await asyncio.gather(*tasks)
+    end_time = time.time()
+    print('Async request took:', end_time - start)
+
 
 if __name__ == "__main__":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main())
