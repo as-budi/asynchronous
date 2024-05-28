5e071fd127b4b56809d0d62eade344864371220f
as-budi <agungsetiabudi@ub.ac.id>
2024-05-28 08:39:37 +0700

    add asyncio task

--- async_sleep.py
+++ async_sleep.py
@@ -3,16 +3,17 @@ import time
 
 async def async_sleep(n):
     print("Before sleep", n)
-    await asyncio.sleep(5)
+    await asyncio.sleep(2)
     print("After sleep", n)
 
 async def async_hello():
     print('Hello')
 
 async def main():
+    task = asyncio.create_task(async_sleep(2))
     await async_sleep(1)
-    await async_sleep(2)
     await async_hello()
+    await task
 
 if __name__ == "__main__":
     s = time.time()
